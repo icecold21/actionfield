23571d8d57af1d9a08d077147d706b6145ca4872
Jonathan Wylliem <jonathan.wylliem@jurnal.id>
2017-11-13 19:41:21 +0700

    Finalize spec

--- spec/dummy/db/migrate/20171113095811_create_scooby_doos.rb
+++ spec/dummy/db/migrate/20171113095811_create_scooby_doos.rb
@@ -3,6 +3,7 @@ class CreateScoobyDoos < ActiveRecord::Migration[5.1]
     create_table :scooby_doos do |t|
       t.string :name
       t.text :description
+      t.integer :legs
       t.timestamps
     end
   end
--- spec/models/scooby_doo_spec.rb
+++ spec/models/scooby_doo_spec.rb
@@ -1,10 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe ScoobyDoo, type: :model do
-  describe 'ActionField' do
-    context 'test action field' do
-      it 'called the right method on field' do
-        scooby_doo = ScoobyDoo.new(name: "ABC")
+  describe "test ActionField" do
+    context "test action field" do
+      it "called the right method on field" do
+        scooby_doo = ScoobyDoo.new(name: "Schooby")
+        scooby_doo.save
+        expect(scooby_doo.name).to eq "schooby"
+      end
+
+      it "called the right method on field" do
+        scooby_doo = ScoobyDoo.new(name: "schooby")
+        scooby_doo.save
+        expect(scooby_doo.name).not_to eq "schoobyy"
       end
     end
   end
