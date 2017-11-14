d213b3be8912557b6004e741b012f1d3f2a8f80a
Jonathan Wylliem <jonathan.wylliem@jurnal.id>
2017-11-14 11:15:31 +0700

    Changes name

--- lib/actionfield.rb
+++ lib/actionfield.rb
@@ -1,5 +1,5 @@
-require 'action_field/core_ext'
-require 'action_field/acts_as_action_field'
+require 'actionfield/core_ext'
+require 'actionfield/acts_as_action_field'
 
 
 puts "ActionField Loaded"
